be73ba63b0385b881eec264605c47b2647c70e89
beemhuse <brightawah94@gmail.com>
2026-04-27 18:56:04 +0100

    chore: added husky/commitlint

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+    },
+  },
+];
