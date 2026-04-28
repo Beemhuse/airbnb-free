c0d020db6aa9804696e820ab1f9c966cd9733283
beemhuse <brightawah94@gmail.com>
2026-04-28 17:12:44 +0100

    chore: updated the favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,20 +9,14 @@ import Footer from '@/components/Footer'
 export const metadata: Metadata = {
   title: 'Airbnb - Onboarding',
   description: 'Complete your Airbnb account setup',
-  generator: 'v0.app',
   icons: {
     icon: [
+     
+     
       {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '/airbnb-logo.png',
+        type: 'image/png',
+        sizes: '16x16 32x32'
       },
     ],
     apple: '/apple-icon.png',
